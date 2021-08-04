66b7c3b0833a1bca89d35506c0e552142ad555f2
Pranavchandaliya <43096879+Pranavchandaliya@users.noreply.github.com>
2021-08-04 11:15:38 +0530

    updates

--- application.py
+++ application.py
@@ -3,15 +3,15 @@ import sys
 import os
 from werkzeug.utils import secure_filename
 from predict import predict_covid
-app = Flask(__name__)
+application = Flask(__name__)
 
 
-@app.route('/')
+@application.route('/')
 def upload():
     return render_template("index.html")
 
 
-@app.route('/success', methods=['POST'])
+@application.route('/success', methods=['POST'])
 def success():
     if request.method == 'POST':
         f = request.files['file']
@@ -31,4 +31,4 @@ def success():
 
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    application.run(debug=True)
